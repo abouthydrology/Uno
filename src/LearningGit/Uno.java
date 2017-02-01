b3e455cf1bdcd7f90a95aad386d1df567b301156
riccardo <riccardo.rigon@unitn.it>
2017-02-01 15:08:07 +0100

    first commit

---
+++ src/LearningGit/Uno.java
@@ -0,0 +1,12 @@
+package LearningGit;
+
+public class Uno {
+    private static double a=1;
+    private static double  b=1;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("One plus one is "+(a+b));
+
+	}
+
+}
